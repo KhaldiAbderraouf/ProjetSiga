bb22697e5f0ef38674cc66cd66f844491ef66e2b
Harouit Chems Eddine <fc_harouit@esi.dz>
2018-12-30 22:19:35 +0100

    added GlobalIgnore

--- src/View/BDDLiveTest.java
+++ src/View/BDDLiveTest.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 
 public class MainClassTesting {
     public static void main(String[] args) throws SQLException {
-        Colonne colonne = Colonne.dbFetchWithID(5);
+        Colonne colonne = Colonne.dbFetchWithID(4);
         System.out.println("ID : "+ colonne.id+"| Nom : "+ colonne.nom +"| IDTable : "+ colonne.idTableAttr +";");
         colonne.idTableAttr = 1;
         colonne.dbModifier();
-        ArrayList<Colonne> listColonne = Colonne.dbGetAll();
+        ArrayList<Colonne> listColonne = (ArrayList<Colonne>) Colonne.dbGetAll();
         for (Colonne c:listColonne) {
             System.out.println("ID : "+ c.id+"| Nom : "+ c.nom +"| IDTable : "+ c.idTableAttr +";");
         }
