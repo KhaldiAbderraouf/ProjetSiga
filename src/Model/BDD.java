7b9dd1580fb1d4b5bdb8776d7267fc7ecf531f92
khaldi abderraouf <fr_khaldi@esi.dz>
2018-12-30 21:36:10 +0100

    merge bdd and master

--- src/Model/BDD.java
+++ src/Model/BDD.java
@@ -1,7 +1,23 @@
 package Model;
 import Controler.Couche;
 
+import java.sql.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
 public class BDD extends Loader{
+
+    private static final String    DBName="SIGDB";
+    private static final String    DBUrl="jdbc:mariadb://localhost:3306/"+DBName;
+	private static final String    JDBC_DRIVER = "com.mariadb.jdbc.Driver";
+    private static final String    DBUserName="root";
+    private static final String    DBPasswd="root";
+    private static final int       DBPort=3096;
+    private static final String    DBServerName="root";
+
+    private static ResultSet generatedKeys;
+
 	public void setUser(User user)
 	{
 		
@@ -47,4 +63,75 @@ public class BDD extends Loader{
 		return null;
 		
 	}
+
+	private static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(DBUrl, DBUserName, DBPasswd);
+		return conn;
+	}
+
+    public static ResultSet execute(String query, List<String> args){
+
+        PreparedStatement preparedStatement = null;
+        ResultSet querySet = null;
+        try(Connection connection= BDD.getConnection()) {
+            connection.setAutoCommit(false);
+            int cpt=1;
+            preparedStatement = connection.prepareStatement(query);
+            if(args != null)
+                for (Iterator<String> i = args.iterator(); i.hasNext();) {
+                    String arg = i.next();
+                    preparedStatement.setString(cpt, arg);
+                    cpt++;
+                }
+            querySet = preparedStatement.executeQuery();
+            connection.commit();
+            preparedStatement.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            return querySet;
+        }
+    }
+
+    public static ResultSet executeTransaction(List<String> queries,List<List<String>> argsList){
+        ResultSet querySet = null;
+        try(Connection connection= BDD.getConnection()) {
+            connection.setAutoCommit(false);
+            Iterator<List<String>> argsListIt = argsList.iterator();
+            PreparedStatement preparedStatement = null;
+            for (Iterator<String> queryIt = queries.iterator(); queryIt.hasNext();){
+                List<String> args = argsListIt.next();
+                int cpt=1;
+                String query = queryIt.next();
+                preparedStatement = connection.prepareStatement(query);
+                if(args != null)
+                    for (Iterator<String> i = args.iterator(); i.hasNext();) {
+                        String arg = i.next();
+                        preparedStatement.setString(cpt, arg);
+                        cpt++;
+                    }
+                querySet = preparedStatement.executeQuery();
+            }
+            connection.commit();
+            if(preparedStatement != null)preparedStatement.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            return querySet;
+        }
+    }
+
+
+
+    public static ResultSet fetchAll(String query, List<String> args){
+
+        return execute(query, args);
+    }
+
+    public static ResultSet fetch(String query, List<String> args){
+
+        return fetchAll(query, args);
+
+    }
+
 }
--- src/Model/Colonne.java
+++ src/Model/Colonne.java
@@ -4,9 +4,18 @@ import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.ArrayList;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Colonne {
     private int taille;
     private String name;
+    public int id;
+    public String nom;
+    public int idTableAttr;
+
     private ArrayList<String> col = new ArrayList<String>();
 
     public Colonne(String name,int taille, Object ... list){
@@ -80,6 +89,73 @@ public class Colonne {
             }
         }
         return res;
+	}
+    
+    public Colonne(int id, String nom, int idTableAttr){
+        this.id = id;
+        this.nom = nom;
+        this.idTableAttr = idTableAttr;
+    }
+
+    public Colonne(String nom, int idTableAttr){
+        this.nom = nom;
+        this.idTableAttr = idTableAttr;
+    }
+
+    private static List adaptResultSetToArrayList(ResultSet rs){
+        List list = new ArrayList<Colonne>();
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String nom = rs.getString("NOM");
+                int idTable = rs.getInt("IDTableAttr");
+                Colonne colonne = new Colonne(id, nom, idTable);
+                list.add(colonne);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    public static List dbGetAll(){
+        String query = "SELECT * FROM Colonne";
+        ResultSet rs = BDD.fetchAll(query, null);
+        List list= adaptResultSetToArrayList(rs);
+        return list;
+    }
+
+    public static List dbGetAllWithIDTableAttr(int idTableAttr){
+        String query = "SELECT * FROM Colonne WHERE IDTableAttr = ?";
+        ResultSet rs = BDD.fetchAll(query, Arrays.asList(String.valueOf(idTableAttr)));
+        List list= null;
+        list = adaptResultSetToArrayList(rs);
+        return list;
+    }
+
+
+    public void dbAjouter(){
+        String query = "INSERT INTO Colonne VALUES(null, ?, ?)";
+        BDD.execute(query, Arrays.asList(this.nom, String.valueOf(this.idTableAttr)));
+    }
+
+    public void dbModifier(){
+        String query = "UPDATE Colonne SET Nom = ?, IDTableAttr = ? WHERE ID = ?";
+        ArrayList<String> args = new ArrayList<String>();
+        args.add(this.nom);
+        args.add(String.valueOf(this.idTableAttr));
+        args.add(String.valueOf(this.id));
+        BDD.execute(query, args);
+    }
+
+    public static Colonne dbFetchWithID(int idToFetch){
+        Colonne colonne;
+        String query = "SELECT * FROM Colonne WHERE ID = ?";
+        ResultSet rs = BDD.fetch(query, Arrays.asList(String.valueOf(idToFetch)));
+        List<Colonne> list= adaptResultSetToArrayList(rs);
+        colonne = new Colonne(list.get(0).id, list.get(0).nom, list.get(0).idTableAttr);
+        return colonne;
     }
 
 }
